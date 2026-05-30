f97572fcc6cc98b94b10a66bc24b38855c52d9cf
Justin <jcasp425@gmail.com>
2026-05-29 22:07:42 -0400

    Switched to using tagged union Values.

--- clox/include/value.h
+++ clox/include/value.h
@@ -3,7 +3,19 @@
 
 #include "common.h"
 
-typedef double Value;
+typedef enum {
+    VAL_NIL,
+    VAL_BOOL,
+    VAL_NUMBER
+} ValueType;
+
+typedef struct {
+    ValueType type;
+    union {
+        bool boolean;
+        double number;
+    } as;
+} Value;
 
 typedef struct {
     unsigned int count;
@@ -11,9 +23,21 @@ typedef struct {
     Value * values;
 } ValueArray;
 
+#define IS_NIL(value)    ((value).type == VAL_NIL)
+#define IS_BOOL(value)   ((value).type == VAL_BOOL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+
+#define AS_BOOL(value)   ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+#define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
+#define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+
 void value_array_init(ValueArray * array);
 void value_array_write(ValueArray * array, Value value);
 void value_array_free(ValueArray * array);
 void value_print(Value value);
+bool value_equality(Value a, Value b);
 
 #endif /* CLOX_VALUE_H */
